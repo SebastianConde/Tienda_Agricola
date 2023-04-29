ba427972991ce76ffea8e4b6f9cdd54265c7a6c4
chenaoh <luissebastian.conde@utp.edu.co>
2023-04-29 17:21:28 -0500

    Tienda Ecologica

--- ControlPlagas.py
+++ ControlPlagas.py
@@ -3,7 +3,7 @@ from ProductoControl import ProductoControl
 class ControlPlagas(ProductoControl):
     __lista_plagas = []
     def __init__(self):
-        producto_control = super().__init__()
+        super().__init__()
         self.__periodo_carencia = str(input("Ingrese el periodo de carencia del control de plagas: "))
         self.__class__.__lista_plagas.append(self)
         print("-----------------------------")
